cc5fe2e19cbec38ea923227afa92ecba8647c258
mathildaHERR <mathilda.herrell@gmail.com>
2023-11-18 16:10:04 -0500

    debut battlebar

--- site-web/src/pages/Index.tsx
+++ site-web/src/pages/Index.tsx
@@ -1,9 +1,21 @@
+import '../assets/css/indexStyles.css'
+//import { NavLink } from "react-router-dom";
+
 import React from "react"
 
 export default function Index() : JSX.Element {
     return (
         <div>
-            Hello World!
+           {/* Hello World! */}
+            <div className='countdown'>
+                <p>Time left to complete tasks</p>
+                <p>xx:xx:xx</p>
+            </div>
+            <div id="battleBarContainer">
+                <span>naughty</span>
+                <div className='battleBar'></div>
+                <span>nice</span>
+            </div>
         </div>
     );
 }
