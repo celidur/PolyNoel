3c00ad52dd52468ab21750678756fae0f2facee7
DaCondor54 <49108155+DaCondor54@users.noreply.github.com>
2023-11-18 14:57:48 -0500

    Added mini-task-list component

--- site-web/src/components/Navbar.tsx
+++ site-web/src/components/Navbar.tsx
@@ -1,16 +1,17 @@
 import Logo from '../assets/img/test-logo.png'
 import { NavLink } from "react-router-dom";
+import NavBarCSS from './Navbar.module.css'
 
 export default function NavBar() : JSX.Element {
   return (
-    <header>
+    <header className={NavBarCSS.header}>
         <div>
-          <NavLink className="title" to="/">
-            <img className="logo" src={Logo} alt="logo" />        
+          <NavLink className={NavBarCSS.title} to="/">
+            <img className={NavBarCSS.logo} src={Logo} alt="logo" />        
             PolyNoel
           </NavLink>          
         </div>
-        <NavLink className="title" to="/parent">
+        <NavLink className={NavBarCSS.title} to="/parent">
           <button>Switch to Parent Mode</button> 
         </NavLink>          
     </header>
--- site-web/src/components/TaskLIst.tsx
+++ site-web/src/components/TaskLIst.tsx
@@ -1,9 +1,19 @@
-import Logo from '../assets/img/test-logo.png'
+import Task from "./Task";
+import TaskListCSS from './TaskList.module.css'
 
-export default function TaskList() : JSX.Element {
+type TaskListProps = { taskNames : string[], title : string }
+
+export default function TaskList(props : TaskListProps) : JSX.Element {
   return (
-    <div>
-        
+    <div className={TaskListCSS.container}>
+        <div className={TaskListCSS.title}>
+            {props.title}
+        </div>
+        <div className={TaskListCSS.tasklist}>
+            {props.taskNames.map((taskName, i) =>
+                <Task key={i} taskname={taskName}></Task>
+            )}
+        </div>
     </div>
   );
 }
