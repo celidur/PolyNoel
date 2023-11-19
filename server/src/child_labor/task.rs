5f098bd084c2f3e54d7d5c92e99ef2876a0e4480
lemark <marcantoine_manningham@outlook.com>
2023-11-18 19:47:44 -0500

    Add remove and add task

--- server/src/child_labor/task.rs
+++ server/src/child_labor/task.rs
@@ -1,13 +1,49 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 type Day = u16;
 
 #[derive(Serialize, Deserialize, JsonSchema, Debug, Default)]
 pub struct Task {
-    name: String,
-    recurrent: bool,
+    pub id: String,
+    pub name: String,
     /// This is a string representing a date
+    pub deadline: String,
+    pub recurrent_interval: Option<Day>,
+}
+
+impl Task {
+    const fn new(
+        id: String,
+        name: String,
+        deadline: String,
+        recurrent_interval: Option<Day>,
+    ) -> Self {
+        Self {
+            id,
+            name,
+            deadline,
+            recurrent_interval,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, JsonSchema, Debug, Default)]
+pub struct CreateTask {
+    name: String,
     deadline: String,
     recurrent_interval: Option<Day>,
+}
+
+impl CreateTask {
+    pub fn build(self) -> Task {
+        let id = Uuid::new_v4();
+        Task::new(
+            id.to_string(),
+            self.name,
+            self.deadline,
+            self.recurrent_interval,
+        )
+    }
 }
--- server/src/child_labor/tasks.rs
+++ server/src/child_labor/tasks.rs
@@ -1,4 +1,4 @@
-use super::task::Task;
+use super::task::{CreateTask, Task};
 use crate::common::state::App;
 use aide::axum::IntoApiResponse;
 use axum::{extract::State, http::StatusCode};
@@ -9,7 +9,26 @@ pub struct Tasks {
     tasks: Vec<Task>,
 }
 
-pub async fn add_task(State(app): State<App>, Json(task): Json<Task>) -> impl IntoApiResponse {
-    app.users.lock().await.tasks.tasks.push(task);
+impl Tasks {
+    fn push(&mut self, task: CreateTask) {
+        self.tasks.push(task.build());
+    }
+}
+
+pub async fn add_task(
+    State(app): State<App>,
+    Json(task): Json<CreateTask>,
+) -> impl IntoApiResponse {
+    app.users.lock().await.tasks.push(task);
     StatusCode::CREATED
+}
+
+pub async fn remove_task(State(app): State<App>, Json(id): Json<String>) -> impl IntoApiResponse {
+    let tasks = &mut app.users.lock().await.tasks.tasks;
+    if let Some(i) = tasks.iter().position(|t| t.id == id) {
+        tasks.remove(i);
+        StatusCode::OK
+    } else {
+        StatusCode::NOT_FOUND
+    }
 }
