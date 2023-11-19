a718db1f03a8f5bb580797859b72871a322cb31a
ZacharyOuellet <113061957+ZacharyOuellet@users.noreply.github.com>
2023-11-19 14:09:33 -0500

    remove ghost drag

--- site-web/src/ToyCatalog/DraggableToyCard.tsx
+++ site-web/src/ToyCatalog/DraggableToyCard.tsx
@@ -1,20 +1,26 @@
-import React, { useState } from "react"
+import React, { useEffect } from "react"
 import styles from "../assets/css/ToyCatalog.module.css"
 import { useDrag } from "react-dnd";
 import { ItemTypes } from "../Constants";
 import ToyCard from "../ToyCatalog/ToyCard";
 import { ToyCardProps } from "../ToyCatalog/ToyCard";
+import { getEmptyImage } from 'react-dnd-html5-backend'
+
 
 
 
 export default function DraggableToyCard(props : ToyCardProps) : JSX.Element {
-    const [{isDragging}, drag] = useDrag(() => ({
+    const [{isDragging}, drag, preview] = useDrag(() => ({
         type: ItemTypes.TOYCARD,
         item: props,
         collect: (monitor) => ({
             isDragging: !!monitor.isDragging()
         })
-    }))
+    }));
+
+    useEffect(() => {
+        preview(getEmptyImage(), { captureDraggingState: true })
+      }, [])
     return(
         <div ref={drag} className={styles.toyCard} style={{opacity: isDragging? "0": "1",}}>
             <ToyCard {...props}/>
