e9db9ff02836a57e69101ff096e4471b79d1bf1e
ZacharyOuellet <113061957+ZacharyOuellet@users.noreply.github.com>
2023-11-18 23:34:12 -0500

    card queue

--- site-web/src/pages/ToyCatalog.tsx
+++ site-web/src/pages/ToyCatalog.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { DndProvider, useDrop } from 'react-dnd'
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { ItemTypes } from "../Constants";
@@ -14,67 +14,84 @@ export default function ToyCatalog() : JSX.Element {
     );
 }
 
+function loadToys(amount : number) : ToyCardProps[] {
+    return [
+        {id:"1",title:"Test1", imgSrc:"test", difficulty:5},
+        {id:"2", title:"Test2", imgSrc:"test", difficulty:4}
+    ]
+}
 
 function TinderGame() : JSX.Element {
-
-    const [current, setCurrent] = useState<ToyCardProps>(
-        {id:"1",title:"Test", imgSrc:"test", difficulty:5});
-    const [next, setNext] = useState<ToyCardProps>(
-        {id:"2", title:"Test2", imgSrc:"test", difficulty:4}
-    );
-    const switchCards = () =>{
-        const temp = current;
-        setCurrent(next);
-        setNext(temp);
-    }
+    const [cards, setCards] = useState<ToyCardProps[]>(loadToys(2));
+    const [switchFlag, setSwitchFlag] = useState<boolean>(false);
+    useEffect(() => {
+        console.log("Switch flag changed");
+        if(switchFlag) {
+            console.log("Switching cards");
+            console.log(cards);
+            cards.shift();
+            if (cards.length < 2) {
+                cards.push(...loadToys(5));
+            }
+            setCards(cards);
+            setSwitchFlag(false);
+        }
+    }, [switchFlag]);
 
     return (
         <>
             <div className={styles.cardDragContainer}>
-                <RefuseSquare/>
-                <ToyCard {...current}/>
-                <AcceptSquare callback={switchCards}/>
+                <RefuseSquare callback={setSwitchFlag} currentCard={cards[0]}/>
+                <ToyCard {...cards[0]}/>
+                <AcceptSquare callback={setSwitchFlag} currentCard={cards[0]}/>
             </div>
 
         </>
     )
 }
 
-function accept(toy:ToyCardProps, callback : ()=>void) : void {
+function accept(toy:ToyCardProps, callback :  (setSwitchFlag:boolean)=>void) : void {
     console.log("Accepted : " + toy.title);
-    callback();
+    callback(true);
 }
-
-function AcceptSquare({callback}: {callback: ()=>void}) : JSX.Element {
-    const [,drop] = useDrop(
-        () => ({
-            accept: ItemTypes.TOYCARD,
-            drop: (_,monitor) => accept(monitor.getItem(), callback),
-        })
-    )
-    return(
-        <div ref={drop} style={{width: "150px", backgroundColor: "limegreen"}}>
-
-        </div>
-    )
+interface DecisionSquareProps {
+    callback: (setSwitchFlag:boolean)=>void,
+    currentCard: ToyCardProps
 }
-
-function refuse(toy:ToyCardProps) : void {
-    console.log("Refused : " + toy.title)
-}
-
-function RefuseSquare() : JSX.Element {
+function AcceptSquare({callback, currentCard}: DecisionSquareProps) : JSX.Element {
     const [{isOver},drop] = useDrop(
         () => ({
             accept: ItemTypes.TOYCARD,
-            drop: (_,monitor) => refuse(monitor.getItem()),
+            drop: () => accept(currentCard, callback),
             collect: (monitor) => ({
                 isOver: !!monitor.isOver()
             })
         })
     )
     return(
-        <div ref={drop} style={{width: "150px", backgroundColor: "red"}}>
+        <div ref={drop} style={{width: "150px", backgroundColor: isOver?"limegreen" : "lightgreen"}}>
+
+        </div>
+    )
+}
+
+function refuse(toy:ToyCardProps, callback : (setSwitchFlag:boolean)=>void) : void {
+    console.log("Refused : " + toy.title);
+    callback(true);
+}
+
+function RefuseSquare({callback, currentCard} : DecisionSquareProps) : JSX.Element {
+    const [{isOver},drop] = useDrop(
+        () => ({
+            accept: ItemTypes.TOYCARD,
+            drop: (_,monitor) => refuse(monitor.getItem(), callback),
+            collect: (monitor) => ({
+                isOver: !!monitor.isOver()
+            })
+        })
+    )
+    return(
+        <div ref={drop} style={{width: "150px", backgroundColor: isOver?"red":"pink"}}>
             
         </div>
     )
