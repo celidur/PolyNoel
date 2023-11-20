9629b03bd4f38cb5eaaaaad9391b46d7e7fa1382
DaCondor54 <49108155+DaCondor54@users.noreply.github.com>
2023-11-19 22:06:05 -0500

    Added button to go to toy things

--- site-web/src/pages/Index.tsx
+++ site-web/src/pages/Index.tsx
@@ -1,9 +1,10 @@
 import '../assets/css/indexStyles.css'
-//import { NavLink } from "react-router-dom";
+import styles from './Index.module.css';
 
 import React, { useEffect, useState } from "react"
 import HTTPManager, { Task, TaskStatus } from '../assets/js/http_manager';
 import TaskList from '../components/TaskList';
+import { NavLink } from 'react-router-dom';
 
 export default function Index() : JSX.Element {
     const [tasks, setTasks] = useState<Task[]>([]);
@@ -36,17 +37,31 @@ export default function Index() : JSX.Element {
     };
 
     return (
-        <div>
+        <div className={styles.container}>
            {/* Hello World! */}
-            <div className='countdown'>
-                <p>Time left to complete tasks</p>
-                <p>xx:xx:xx</p>
-            </div>
-            <div id="battleBarContainer">
-                <span>naughty</span>
-                <div className='battleBar'></div>
-                <span>nice</span>
-            </div>
+           <div className={styles.battlepass_container}>
+                <div className='countdown'>
+                    <p>Time left to complete tasks</p>
+                    <p>xx:xx:xx</p>
+                </div>
+                <div id="battleBarContainer">
+                    <span>naughty</span>
+                    <div className='battleBar'></div>
+                    <span>nice</span>
+                </div>
+           </div>
+           <div>
+                <div className={styles.title}>TOYS THINGS</div>
+                <div className={styles.toys_container}>
+                    Something
+                </div>
+                <div className={styles.button_container}>
+                    <NavLink to="/toycatalog">
+                        <button className={styles.button}>Add Ideas</button>
+                    </NavLink>
+                    <button className={styles.button}>Rank Ideas</button>                    
+                </div>
+           </div>
             <TaskList tasks={tasks} title="Tasks" taskType={"view-task"} onTaskClick={ changeTaskState }></TaskList>
         </div>
     );
