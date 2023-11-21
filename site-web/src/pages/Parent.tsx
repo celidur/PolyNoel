93107fc1d0c7295aeb48bcb3ecea2087833ca3d5
GitHubYanis <yanis21p@gmail.com>
2023-11-21 10:06:04 -0500

    Changed style

--- site-web/src/pages/Parent.tsx
+++ site-web/src/pages/Parent.tsx
@@ -5,17 +5,20 @@ export default function Parent() : JSX.Element {
     return (
         <>
             <div className={styles.container}>
+                <div className={styles.pageTile}>
+                    Settings
+                </div>
                 <NavLink className={styles.feature} to="/parent/tasks">
-                    Tasks
+                    Edit Tasks
                 </NavLink>
                 <NavLink className={styles.feature} to="/parent/battlepass">
-                    BattlePass
+                    Edit BattlePass
                 </NavLink>
                 <NavLink className={styles.feature} to="/parent/categories">
                     Filter Categories
                 </NavLink>
                 <NavLink className={styles.feature} to="/parent/settings">
-                    Settings
+                    Ajust Price Range
                 </NavLink>
             </div>            
         </>
