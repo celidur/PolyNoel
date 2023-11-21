103530b35ff675622f5123981f5dfc7c3ac62d66
gaetan <github@gagolino.com>
2023-11-21 02:27:25 -0500

    fix img

--- build.sh
+++
@@ -1,4 +0,0 @@
-docker build ./server/ --file ./server/Dockerfile --tag ghcr.io/celidur/polynoel-server-ghcr:lastest
-docker push ghcr.io/celidur/polynoel-server-ghcr:lastest
-docker build ./site-web/ --file ./site-web/Dockerfile --tag ghcr.io/celidur/polynoel-client-ghcr:lastest
-docker push ghcr.io/celidur/polynoel-client-ghcr:lastest
